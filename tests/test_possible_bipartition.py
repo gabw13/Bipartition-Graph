db38388ffa66c9d0e2c0f55bd9bf9efd633b8caf
char <charmara.mays@adadevelopersacademy.org>
2022-09-22 15:32:47 -0700

    update test cases

--- tests/test_possible_bipartition.py
+++ tests/test_possible_bipartition.py
@@ -3,12 +3,13 @@ from graphs.possible_bipartition import possible_bipartition
 
 def test_example_1():
     # Arrange
-    dislikes = [ [],
-      [2, 3],
-      [1, 4],
-      [1],
-      [2]
-    ]
+    dislikes = {
+      "Fido": [],
+      "Rufus": ["James", "Alfie"],
+      "James": ["Rufus", "T-Bone"],
+      "Alfie": ["Rufus"],
+      "T-Bone": ["James"]
+    }
 
     # Act
     answer = possible_bipartition(dislikes)
@@ -17,12 +18,12 @@ def test_example_1():
     assert answer
 
 def test_example_2():
-    # Arrange
-    dislikes =  [ [],
-      [2, 3],
-      [1, 3],
-      [1, 2]
-    ]
+    dislikes = {
+      "Fido": [],
+      "Rufus": ["James", "Alfie"],
+      "James": ["Rufus", "Alfie"],
+      "Alfie": ["Rufus", "James"]
+    }
 
     # Act
     answer = possible_bipartition(dislikes)
@@ -32,13 +33,14 @@ def test_example_2():
 
 def test_example_r():
     # Arrange
-    dislikes = [ [],
-      [2, 5],
-      [1, 3],
-      [2, 4],
-      [3, 5],
-      [1, 4]
-    ]
+    dislikes = {
+      "Fido": [],
+      "Rufus": ["James", "Scruffy"],
+      "James": ["Rufus", "Alfie"],
+      "Alfie": ["Rufus", "T-Bone"],
+      "T-Bone": ["Alfie", "Scruffy"],
+      "Scruffy": ["Rufus", "T-Bone"]
+    }
 
     # Act
     answer = possible_bipartition(dislikes)
@@ -48,14 +50,15 @@ def test_example_r():
 
 def test_will_return_true_for_a_graph_which_can_be_bipartitioned():
     # Arrange
-    dislikes = [ [3, 6],
-      [2, 5],
-      [1, 3],
-      [0, 2],
-      [5],
-      [1, 4],
-      [0]
-    ]
+    dislikes = {
+      "Fido": ["Alfie", "Bruno"],
+      "Rufus": ["James", "Scruffy"],
+      "James": ["Rufus", "Alfie"],
+      "Alfie": ["Fido", "James"],
+      "T-Bone": ["Scruffy"],
+      "Scruffy": ["Rufus", "T-Bone"],
+      "Bruno": ["Fido"]
+    }
 
     # Act
     answer = possible_bipartition(dislikes)
@@ -65,14 +68,15 @@ def test_will_return_true_for_a_graph_which_can_be_bipartitioned():
 
 def test_will_return_false_for_graph_which_cannot_be_bipartitioned():
     # Arrange
-    dislikes = [ [3, 6],
-      [2, 5],
-      [1, 3],
-      [0, 2, 4],
-      [3, 5],
-      [1, 4],
-      [0]
-    ]
+    dislikes = {
+      "Fido": ["Alfie", "Bruno"],
+      "Rufus": ["James", "Scruffy"],
+      "James": ["Rufus", "Alfie"],
+      "Alfie": ["Fido", "James", "T-Bone"],
+      "T-Bone": ["Alfie", "Scruffy"],
+      "Scruffy": ["Rufus", "T-Bone"],
+      "Bruno": ["Fido"]
+    }
 
     # Act
     answer = possible_bipartition(dislikes)
@@ -82,20 +86,82 @@ def test_will_return_false_for_graph_which_cannot_be_bipartitioned():
 
 
 def test_will_return_true_for_empty_graph():
-    assert possible_bipartition([])
+    assert possible_bipartition({})
   
 def test_will_return_false_for_another_graph_which_cannot_be_bipartitioned():
     # Arrange
-    dislikes = [ [3, 6],
-      [2, 5],
-      [1, 3],
-      [0, 2, 4],
-      [3, 5],
-      [1, 4],
-      [0],
-      [8],
-      [7]
-    ]
+    dislikes = {
+      "Fido": ["Alfie", "Bruno"],
+      "Rufus": ["James", "Scruffy"],
+      "James": ["Rufus", "Alfie"],
+      "Alfie": ["Fido", "James", "T-Bone"],
+      "T-Bone": ["Alfie", "Scruffy"],
+      "Scruffy": ["Rufus", "T-Bone"],
+      "Bruno": ["Fido"],
+      "Spot": ["Nala"],
+      "Nala": ["Spot"]
+    }
+
+    # Act
+    answer = possible_bipartition(dislikes)
+
+    # Assert
+    assert not answer
+
+def test_multiple_dogs_at_beginning_dont_dislike_any_others():
+  # Arrange
+    dislikes = {
+      "Fido": [],
+      "Rufus": [],
+      "James": [],
+      "Alfie": ["T-Bone"],
+      "T-Bone": ["Alfie", "Scruffy"],
+      "Scruffy": ["T-Bone"],
+      "Bruno": ["Nala"],
+      "Spot": ["Nala"],
+      "Nala": ["Bruno", "Spot"]
+    }
+
+    # Act
+    answer = possible_bipartition(dislikes)
+
+    # Assert
+    assert answer
+
+
+def test_multiple_dogs_in_middle_dont_dislike_any_others():
+    # Arrange
+    dislikes = {
+      "Fido": ["Alfie"],
+      "Rufus": ["James", "Scruffy"],
+      "James": ["Rufus", "Alfie"],
+      "Alfie": ["Fido", "James"],
+      "T-Bone": [],
+      "Scruffy": ["Rufus"],
+      "Bruno": [],
+      "Spot": ["Nala"],
+      "Nala": ["Spot"]
+    }
+
+    # Act
+    answer = possible_bipartition(dislikes)
+
+    # Assert
+    assert answer
+
+def test_will_return_false_for_disconnected_graph_which_cannot_be_bipartitioned():
+    # Arrange
+    dislikes = {
+      "Ralph": ["Tony"],
+      "Tony": ["Ralph"],
+      "Fido": ["Alfie", "Bruno"],
+      "Rufus": ["James", "Scruffy"],
+      "James": ["Rufus", "Alfie"],
+      "Alfie": ["Fido", "James", "T-Bone"],
+      "T-Bone": ["Alfie", "Scruffy"],
+      "Scruffy": ["Rufus", "T-Bone"],
+      "Bruno": ["Fido"]
+    }
 
     # Act
     answer = possible_bipartition(dislikes)
