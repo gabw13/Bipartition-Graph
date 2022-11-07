01c4b869298be41c5209a2cdf7a6c3543745af19
Gabriela Webb <gabw813@gmail.com>
2022-11-07 00:54:36 -0500

    passes 7/10 tests

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -8,5 +8,57 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
 
+    if len(dislikes) == 0:
+        return True
+
+    all_puppies = list(dislikes.keys())
+    first_pup = all_puppies[0]
+    # if len(dislikes) == 1:
+    #     return [first_pup]
+
+    queue = [first_pup]
+    visited = set()
+    groups = {
+        first_pup: "A"
+    }
+    
+
+    # i = 0
+    for pup in all_puppies:
+        while queue:
+        # while i < len(queue):
+            current = queue.pop(0)
+            opposite_group = "B" if groups[current] == "A" else "A"
+            # i += 1
+            visited.add(current)
+            
+            # for pup in dislikes[current]:
+            if dislikes[current]:
+                for pup in dislikes[current]:
+                    # print(f"current:{current}")
+                    # print(f"pup:{pup}")
+                    if pup not in visited:
+                        queue.append(pup)
+                        # visited.add(pup)
+                        groups[pup] = opposite_group
+                    elif groups[current] == groups[pup]:
+                        return False
+        
+    return True
+    # return visited
+
+dislikes = {
+    "Ralph": ["Tony"],
+    "Tony": ["Ralph"],
+    "Fido": ["Alfie", "Bruno"],
+    "Rufus": ["James", "Scruffy"],
+    "James": ["Rufus", "Alfie"],
+    "Alfie": ["Fido", "James", "T-Bone"],
+    "T-Bone": ["Alfie", "Scruffy"],
+    "Scruffy": ["Rufus", "T-Bone"],
+    "Bruno": ["Fido"]
+}
+    
+
+print (possible_bipartition(dislikes))
