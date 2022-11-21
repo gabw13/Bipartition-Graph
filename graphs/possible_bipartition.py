b2ae037d2e5d3d45f0476215c980558433e0e660
Gabriela Webb <gabw813@gmail.com>
2022-11-21 00:10:22 -0500

    passes all tests

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -5,60 +5,35 @@ def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
         into the same partition.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(N + E) where N is the number of nodes & E is the number of edges
+        Space Complexity: O(N) where N is the number of nodes
     """
 
     if len(dislikes) == 0:
         return True
-
-    all_puppies = list(dislikes.keys())
-    first_pup = all_puppies[0]
-    # if len(dislikes) == 1:
-    #     return [first_pup]
-
+    
+    groups = {}
+    for key in dislikes.keys():
+        groups[key] = 0
+    
+    first_pup = list(dislikes.keys())[0]
+    groups[first_pup] = 1
     queue = [first_pup]
     visited = set()
-    groups = {
-        first_pup: "A"
-    }
-    
 
-    # i = 0
-    for pup in all_puppies:
+    for pup in dislikes:
         while queue:
-        # while i < len(queue):
             current = queue.pop(0)
-            opposite_group = "B" if groups[current] == "A" else "A"
-            # i += 1
             visited.add(current)
-            
-            # for pup in dislikes[current]:
+
             if dislikes[current]:
-                for pup in dislikes[current]:
-                    # print(f"current:{current}")
-                    # print(f"pup:{pup}")
-                    if pup not in visited:
-                        queue.append(pup)
-                        # visited.add(pup)
-                        groups[pup] = opposite_group
-                    elif groups[current] == groups[pup]:
+                for dog in dislikes[current]:
+                    if groups[dog] == 0:
+                        groups[dog] = groups[current] + 1
+                        queue.append(dog)
+                    elif groups[dog] == groups[current]:
                         return False
-        
+        if pup not in visited:
+            queue.append(pup)
     return True
-    # return visited
-
-dislikes = {
-    "Ralph": ["Tony"],
-    "Tony": ["Ralph"],
-    "Fido": ["Alfie", "Bruno"],
-    "Rufus": ["James", "Scruffy"],
-    "James": ["Rufus", "Alfie"],
-    "Alfie": ["Fido", "James", "T-Bone"],
-    "T-Bone": ["Alfie", "Scruffy"],
-    "Scruffy": ["Rufus", "T-Bone"],
-    "Bruno": ["Fido"]
-}
     
-
-print (possible_bipartition(dislikes))
